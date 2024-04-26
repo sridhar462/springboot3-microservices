3df2706a2bd8df5fc495d26709491c5adc7a89d9
sridhar462 <sridhar462@gmail.com>
2024-04-26 15:06:36 +0200

    Initial Commit

---
+++ course-service/src/test/java/com/payil/courseservice/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payil.courseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
